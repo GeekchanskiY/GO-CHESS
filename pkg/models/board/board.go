c495149151e5b8ed42121304b0ff9ee6b6bc7891
GeekchanskiY <astrovskiy.04@gmail.com>
2025-02-02 20:56:10 +0300

    board upd

--- pkg/models/board/board.go
+++ pkg/models/board/board.go
@@ -2,6 +2,12 @@ package board
 
 import "GO-CHESS/pkg/models/pieces"
 
+const (
+	// Size is a board size. Board positions start from 0
+	Size         = 8*8 - 1
+	PiecesAmount = 8 * 2
+)
+
 type IBoard interface {
 	InitBoard() (bool, error)
 	Draw()
@@ -12,4 +18,28 @@ type IBoard interface {
 }
 
 type Board struct {
+	pieces []pieces.IPiece
+	moves  []pieces.IMovement
+}
+
+func (b *Board) NewBoard() Board {
+	initialPieces := make([]pieces.IPiece, 0, PiecesAmount)
+
+	// Set pawns
+	for i := 0; i < 8; i++ {
+		blackPawn := pieces.NewPawn(0, pieces.Black)
+		whitePawn := pieces.NewPawn(0, pieces.White)
+
+		initialPieces = append(initialPieces, &blackPawn)
+		initialPieces = append(initialPieces, &whitePawn)
+	}
+	return Board{}
+}
+
+func (b *Board) GetMoves() []pieces.IMovement {
+	return b.moves
+}
+
+func (b *Board) GetPieces() []pieces.IPiece {
+	return b.pieces
 }
--- pkg/models/board/errors.go
+++ pkg/models/board/errors.go
@@ -5,4 +5,6 @@ import "errors"
 var (
 	GameAlreadyStarted  = errors.New("game already started")
 	GameAlreadyFinished = errors.New("game already finished")
+
+	ErrorOutsideBoard = errors.New("outside board")
 )
